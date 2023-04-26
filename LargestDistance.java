4132cd42420420b1b6364f6de128585a2e142865
Akshay <akshay0127.be21@chitkara.edu.in>
2023-04-26 17:31:13 +0530

    Added Some Java programs

---
+++ LargestDistance.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class LargestDistance {
+    public static void main(String[] args) {
+        int arr[] = { 25, 63, 78, 69, 21 };
+        Arrays.sort(arr);
+        int max = Integer.MIN_VALUE;
+        int diff = 0;
+        
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                
+                diff = arr[j] - arr[i];
+                if (diff > max) {
+                    max = diff;
+                }
+
+            }
+
+        }
+        
+        System.out.println(max);
+    }
+    
+}
--- Motogp.java
+++ Motogp.java
@@ -2,26 +2,23 @@ import java.util.Arrays;
 
 public class Motogp {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 7, 11, 15, 17 };
-        int res =0;
+        int arr[] = { 25,63,78,21,61,69 };
+        int diff =0;
         int min = Integer.MAX_VALUE;
         Arrays.sort(arr);
 
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = i+1; j < arr.length; j++) {
-                res = arr[j] - arr[i];
+                diff = arr[j] - arr[i];
+                if (diff < min) {
+                    min = diff;
+                }
 
             }
 
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (res < min) {
-                min = res;
-
-            }
-
-        }
+       
         System.out.println(min);
         
 
