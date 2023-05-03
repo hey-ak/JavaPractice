bfc52470a69ffa1fc08d382e432fbc31a71962d7
Akshay <akshay0127.be21@chitkara.edu.in>
2023-05-03 14:44:52 +0530

    Added Array intersection problem in java

---
+++ Arrayintersection.java
@@ -0,0 +1,25 @@
+public class Arrayintersection {
+    public static void main(String[] args) {
+      int arr1[] = { 1, 2, 3, 4, 5 };
+      int arr2[] = { 1, 2, 3 };
+
+      if (arr1.length > arr2.length) {
+          for (int i = 0; i < arr1.length; i++) {
+              boolean found = false;
+              for (int j = 0; j < arr2.length; j++) {
+
+                  if (arr1[i] == arr2[j]) {
+                      found = true;
+                      break;
+                  }
+              }
+              if (found) {
+                  System.out.print(arr1[i] + " ");
+              }
+          }
+      }
+      else {
+        System.out.println();
+      }
+    }
+  }
