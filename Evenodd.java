075b51a3215089e616284ca28f6f5f8ea6539628
Akshay <akshay0127.be21@chitkara.edu.in>
2023-05-04 11:20:13 +0530

    Added Basic Even Odd Program in Java Using Functions

---
+++ Evenodd.java
@@ -0,0 +1,17 @@
+public class Evenodd {
+    static void Evenodd(int num) {
+        if (num % 2 == 0) {
+            System.out.println("Even");
+
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        Evenodd(n);
+
+    }
+    
+}
