ccc509cabfb48bcc9d43384548f79e192654687f
Akshay <akshay0127.be21@chitkara.edu.in>
2023-05-04 11:35:58 +0530

    Added Running Sum of Array program in Java

---
+++ RunningSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class RunningSum {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        int result [] = new int[arr.length];
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+
+            sum += arr[i];
+            result[i] = sum;
+
+        }
+        System.out.println(Arrays.toString(result));
+
+        
+    }
+    
+}
